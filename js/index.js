1d6934256898101b352e3e23cbf06a6d082e1514
Michael Goryeb <mgoryeb@Michaels-MacBook-Pro.local>
2018-05-29 17:09:13 -0400

    button click

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+var button = document.getElementById('my-button');
+
+function myFunction() {
+  var title = document.getElementById('page-title');
+
+  title.classList.toggle("turn-green");
+
+  console.dir(title);
+  console.log(title);
+}
+
+button.addEventListener('click', myFunction);
